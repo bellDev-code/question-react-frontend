dc80d9bb8be5593360feb303c01b97825bad7bc5
bellDev-code <sumaoo20@naver.com>
2020-09-27 23:36:32 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: teal;
+`;
+
+const App = () => {
+  return <Container>hello world!</Container>;
+};
+
+export default App;
