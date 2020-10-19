0c6039ee78b60632544103cec3a6839281b89421
bellDev-code <sumaoo20@naver.com>
2020-10-19 15:27:38 +0900

    Footer page css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
-const Wrapper = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 75%;
+  display: flex;
+`;
 
 const Footer = () => {
   return (
