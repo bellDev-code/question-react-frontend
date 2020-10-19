a1ce6a9bdf98dfec3ad47d8e368e2fa27109bcd1
bellDev-code <sumaoo20@naver.com>
2020-10-19 15:23:50 +0900

    desktop main page css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
 import { HashRouter as Router } from "react-router-dom";
 import AppRouter from "./components/Routes";
 import useWindowSize from "./hooks/useWindowSize";
