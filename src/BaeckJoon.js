a2ca4adeb9e4004f8ea9b447d3538c296a09bd6a
bellDev-code <sumaoo20@naver.com>
2020-10-26 15:29:04 +0900

    백준 문제

--- src/BaeckJoon.js
+++ src/BaeckJoon.js
@@ -3,8 +3,23 @@
 //   console.log("강한친구 대한육군");
 // }
 
-// 고양이
-console.log(`\\    /\\
-)  ( ')
-(  /  )
-\\(__)|`);
+// 고양이 출력하기
+// console.log(`\\    /\\
+// )  ( ')
+// (  /  )
+// \\(__)|`);
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ").map((el) => parseInt(el));
+}).on("close", function () {
+  console.log(input[0] + input[1]);
+  process.exit();
+});
